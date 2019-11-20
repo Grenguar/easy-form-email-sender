487724023b3fa6d32a1e3b6b1a3ed14726be55e0
Igor Soroka <stupbird@gmail.com>
2019-11-20 12:55:49 +0200

    add serverless configs for ci/cd and changed travis.yml

--- handler.ts
+++ handler.ts
@@ -10,26 +10,21 @@ const testWebhook: String = "test=test";
 
 const send: Handler = async (event: any, context: Context, callback: Callback) => {
   const isTest = event.body === testWebhook;
-  const decodedBody = decodeURI(event.body);
-  const topic = isTest
-    ? "Test Letter"
-    : `New Lead from GeekExp: ${decodedBody
-        .split("&")[0]
-        .split("=")[1]
-        .replace("+", " ")}`;
+  const decodedBody = decodeURIComponent(event.body);
   const formFields = decodedBody.split("&");
+  const topic = isTest ? "Test Letter" : `New Lead from GeekExp: ${formFields[0].split("=")[1].replace("+", " ")}`;
   const formFieldsHtml: string[] = [];
   formFieldsHtml.push("<h1>Lead Details</h1>");
   formFields.forEach(field => {
-    let newField = "";
+    let newField = field.replace(/=(.*?)/, ": ");
     if (field.match(/(www|http:|https:)+[^\s]+[\w]/) !== null) {
       newField = `<a href="${field}" target="_blank">${field}</a>`;
     } else {
-      newField = `<p>${field.replace("+", " ")}</p><br>`;
+      newField = `<p>${field}</p><br>`;
     }
     formFieldsHtml.push(newField);
   });
-  sendEmail(process.env.TO!, topic, JSON.stringify(formFieldsHtml.join("").replace("=", ": ")), process.env.FROM!);
+  sendEmail(process.env.TO!, topic, JSON.stringify(formFieldsHtml.join("")), process.env.FROM!);
   const response: HelloResponse = {
     statusCode: 200,
     body: "ok"
